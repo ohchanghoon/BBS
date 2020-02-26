29c7246de54c81dba734905551afa00daf0d2827
오창훈 <58061847+ohchanghoon@users.noreply.github.com>
2020-02-26 15:21:26 +0900

    commit

--- WebContent/bbs.jsp
+++ WebContent/bbs.jsp
@@ -1,6 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ page import="java.io.PrintWriter"%>
+<%@ page import="bbs.BbsDAO"%>
+<%@ page import="bbs.Bbs"%>
+<%@ page import="java.util.ArrayList"%>
 <!-- 라이브러리 불러오기 -->
 <!DOCTYPE html>
 <html>
@@ -15,6 +18,10 @@
 		String userID = null;
 		if (session.getAttribute("userID") != null) {
 			userID = (String) session.getAttribute("userID");
+		}
+		int pageNumber = 1;
+		if (request.getParameter("pageNumber") != null) {
+			pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
 		}
 	%>
 	<nav class="navbar navbar-default">
@@ -34,7 +41,7 @@
 				<li class="active"><a href="bbs.jsp">게시판</a></li>
 			</ul>
 			<%
-				if(userID == null){
+				if (userID == null) {
 			%>
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown"><a href="#" class="dropdown-toggle"
@@ -43,11 +50,10 @@
 					<ul class="dropdown-menu">
 						<li><a href="login.jsp">로그인</a></li>
 						<li><a href="join.jsp">회원가입</a></li>
-					</ul>
-				</li>
+					</ul></li>
 			</ul>
 			<%
-				}else{
+				} else {
 			%>
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown"><a href="#" class="dropdown-toggle"
@@ -55,17 +61,17 @@
 					aria-exapnded="false">회원관리<span class="caret"></span></a>
 					<ul class="dropdown-menu">
 						<li><a href="logoutAction.jsp">로그아웃</a></li>
-					</ul>
-				</li>
+					</ul></li>
 			</ul>
-			<%		
+			<%
 				}
 			%>
 		</div>
 	</nav>
 	<div class="container">
 		<div class="row">
-			<table class="table table-striped" style="text-align: center; border: 1px solid #dddddd">
+			<table class="table table-striped"
+				style="text-align: center; border: 1px solid #dddddd">
 				<thead>
 					<tr>
 						<th style="background-color: #eeeeee; text-align: center;">번호</th>
@@ -75,12 +81,20 @@
 					</tr>
 				</thead>
 				<tbody>
+					<%
+						BbsDAO bbsDAO = new BbsDAO();
+						ArrayList<Bbs> list = bbsDAO.getList(pageNumber);
+						for(int i=0; i<list.size(); i++){
+					%>
 					<tr>
 						<td>1</td>
 						<td>안녕하세쇼</td>
 						<td>오창훈</td>
 						<td>2020-01-31</td>
 					</tr>
+					<% 
+						}
+					%>
 				</tbody>
 				<a href="write.jsp" class="btn btn-primary pull-right">글쓰기</a>
 			</table>
--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -4,11 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class BbsDAO {
 
-
 	private Connection conn;
+	private PreparedStatement pstmt;
 	private ResultSet rs;
 
 	public BbsDAO() {
@@ -22,33 +23,87 @@ public class BbsDAO {
 			e.printStackTrace();
 		}
 	}
-	
-	public String getDate() {	// 현재시간을 가져오는 함수
-		String SQL = "select now()";
+
+	public String getDate() { // 현재시간을 가져오는 함수
+		String SQL = "SELECT NOW()";
 		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				return rs.getString(1);
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "";	// 데이터베이스 오류
+		return ""; // 데이터베이스 오류
 	}
-	
-	public int getNext() {	// 현재시간을 가져오는 함수
-		String SQL = "select bbsID from from bbs order by bbsID desc()";
+
+	public int getNext() { // 현재시간을 가져오는 함수
+		String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC()";
 		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				return rs.getString(1) + 1;
+				return rs.getInt(1) + 1;
 			}
-			return 1;
-		}catch (Exception e) {
+			return 1; // 첫번째 게시물인 경우
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "";	// 데이터베이스 오류
+		return -1; // 데이터베이스 오류
+	}
+
+	public int write(String bbsTitle, String userID, String bbsContent) {
+		String SQL = "INSERT INTO BBS VALUE(?, ?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, userID);
+			pstmt.setString(4, getDate());
+			pstmt.setString(5, bbsContent);
+			pstmt.setInt(6, 1);
+			return pstmt.executeUpdate();	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+	public ArrayList<Bbs> getList(int pageNumber){
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY  bbsID DESC  LIMIT 10";
+		ArrayList<Bbs> list = new ArrayList<Bbs>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1,  getNext() - (pageNumber -1) * 10);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	public boolean nextPage(int pageNumber) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY  bbsID DESC  LIMIT 10";
+		ArrayList<Bbs> list = new ArrayList<Bbs>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1,  getNext() - (pageNumber -1) * 10);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
